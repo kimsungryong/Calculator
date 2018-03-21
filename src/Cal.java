9ea94e4c357a2c3a9dcc2dc0e8aaec1f0ff1d82c
KimSungRyong <KimSungRyong@DESKTOP-A8MH29Q>
2018-03-21 23:18:55 +0900

    뺄셈 추가

--- src/Cal.java
+++ src/Cal.java
@@ -4,6 +4,7 @@ public class Cal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("µ¡¼À : " + (5 + 3));
+		System.out.println("»¬¼À: " + (5 + 3));
 	}
 
 }
