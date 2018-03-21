ff1c8c2fea3b34e1053ec8d5f95a5a16015de8ca
KimSungRyong <KimSungRyong@DESKTOP-A8MH29Q>
2018-03-21 23:21:34 +0900

    곱셈 추가 나눗셈 추가

--- src/Cal.java
+++ src/Cal.java
@@ -4,7 +4,9 @@ public class Cal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("µ¡¼À : " + (5 + 3));
-		System.out.println("»¬¼À: " + (5 + 3));
+		System.out.println("»¬¼À: " + (5 - 3));
+		System.out.println("°ö¼À: " + (5 * 3));
+		System.out.println("³ª´°¼À: " + (6 / 3));
 	}
 
 }
